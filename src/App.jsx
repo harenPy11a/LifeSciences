742cabb01b3b2349063a67a0d3f664e86852aff4
Haren Pylla <haren.pylla@gmail.com>
2023-07-25 10:53:58 -0500

    Sections and Components Folders Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
-
+import Home from "./Sections/Home";
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="bg-white">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Sections/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Carousel from "./Components/Carousel";
+function Home() {
+  return <Carousel />;
+}
+
+export default Home;
