acefd7fcad0929f3f512b33ca21b1940364bff90
Sahil K <skhunt125@gmail.com>
2023-08-01 12:38:31 -0500

    Page finished

--- src/Sections/Home.jsx
+++ src/Sections/Home.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import Carousel from "./Components/Carousel";
 import map1 from "./Assets/locationmap1.png";
 import pattern1 from "./Assets/pattern1.jpg";
+import logo1 from "./Assets/wslogo.png";
+import stock1 from "./Assets/stock1.jpg";
 
 function Home() {
   return (
     <div class="bg-gray-200">
       <img
         class="object-cover w-screen h-96 ring-gray-300"
-        src={pattern1}
+        src={stock1}
         alt=""
       ></img>
 
-      <div class="mx-auto max-w-2xl text-center">
+      <div class="mx-auto max-w-2xl text-center mt-16">
         <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           Leave a Message
         </h2>
@@ -106,14 +108,14 @@ function Home() {
                 <select
                   id="country"
                   name="country"
-                  class="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                  class="appearance-none  h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
                 >
                   <option>US</option>
                   <option>CA</option>
                   <option>EU</option>
                 </select>
-                {/* <svg
-                  class="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
+                <svg
+                  class="pointer-events-none absolute right-1 top-0 h-full w-5 text-gray-400"
                   viewBox="0 0 20 20"
                   fill="currentColor"
                   aria-hidden="true"
@@ -123,7 +125,7 @@ function Home() {
                     d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
                     clip-rule="evenodd"
                   />
-                </svg> */}
+                </svg>
               </div>
               <input
                 type="tel"
@@ -181,12 +183,13 @@ function Home() {
             type="submit"
             class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
+            {/* bg-indigo-600 */}
             Let's talk
           </button>
         </div>
       </form>
 
-      <div className="flex flex-row mt-12 ">
+      <div className="flex flex-row mt-16">
         <div className="bg-gray-500 flex-1 h-[40rem]">
           <div class="relative w-full h-[40rem] ">
             <iframe
@@ -200,21 +203,84 @@ function Home() {
           </div>
         </div>
         <div className="bg-[#002e26] flex-1 text-center ">
-          <div className="w-[200px] h-[200px] bg-gray-600 mx-auto mt-16">
-            <p>LOGO</p>
+          <div className="w-[200px] h-[200px] bg-[#747bd4] mx-auto mt-16 rounded-full justify-center flex">
+            <img
+              class="w-[160px] h-[160px] mx-auto my-auto"
+              src={logo1}
+              alt=""
+            ></img>
           </div>
-          <p className="text-white font-[400] text-lg mt-6">Come Visit us At</p>
+          <p className="text-white text-lg mt-6 mb-2">Come visit us at</p>
           <a
             href="https://www.google.com/maps/place/Schaumburg,+IL/@42.032327,-88.2470823,12z/data=!3m1!4b1!4m6!3m5!1s0x880faf88436224e5:0x43bd31cd678f7351!8m2!3d42.0333607!4d-88.0834059!16zL20vMHM1bWQ?entry=ttu"
-            className="text-white font-[400] text-lg mt-6"
+            className="text-white text-lg text-white hover:text-[#72b08e]"
             target="_blank"
           >
             123 Street Lane. Schaumburg IL
           </a>
-          <p className="text-white font-[400] text-lg mt-6">Come Visit us At</p>
-          <p className="text-white font-[400] text-lg mt-6">Come Visit us At</p>
+          <p className="text-white text-lg mt-16">Contact us at</p>
+          <p className="text-white text-lg mt-2">
+            xxx-xxx-xxxx | example@gmail.com
+          </p>
 
-          <p className="text-white font-[300] text-base mt-6">
+          <div className=" flex flex-row justify-center mt-4">
+            <a
+              href=""
+              class="mx-3 text-white hover:text-[#72b08e] group-hover:text-[#72b08e]"
+            >
+              <svg
+                class="h-9 w-9"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                {" "}
+                <path stroke="none" d="M0 0h24v24H0z" />{" "}
+                <rect x="4" y="4" width="16" height="16" rx="4" />{" "}
+                <circle cx="12" cy="12" r="3" />{" "}
+                <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+              </svg>
+            </a>
+            <a
+              href=""
+              class="mx-3 text-white hover:text-[#72b08e]  dark:hover:text-[#72b08e] group-hover:text-white"
+            >
+              <svg
+                class="h-9 w-9 "
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                {" "}
+                <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />{" "}
+                <rect x="2" y="9" width="4" height="12" />{" "}
+                <circle cx="4" cy="4" r="2" />
+              </svg>{" "}
+            </a>
+            <a
+              href=""
+              class="mx-3 text-white hover:text-[#72b08e] dark:hover:text-[#72b08e] group-hover:text-white"
+            >
+              <svg
+                class="w-9 h-9 fill-current"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M12.026 2C7.13295 1.99937 2.96183 5.54799 2.17842 10.3779C1.395 15.2079 4.23061 19.893 8.87302 21.439C9.37302 21.529 9.55202 21.222 9.55202 20.958C9.55202 20.721 9.54402 20.093 9.54102 19.258C6.76602 19.858 6.18002 17.92 6.18002 17.92C5.99733 17.317 5.60459 16.7993 5.07302 16.461C4.17302 15.842 5.14202 15.856 5.14202 15.856C5.78269 15.9438 6.34657 16.3235 6.66902 16.884C6.94195 17.3803 7.40177 17.747 7.94632 17.9026C8.49087 18.0583 9.07503 17.99 9.56902 17.713C9.61544 17.207 9.84055 16.7341 10.204 16.379C7.99002 16.128 5.66202 15.272 5.66202 11.449C5.64973 10.4602 6.01691 9.5043 6.68802 8.778C6.38437 7.91731 6.42013 6.97325 6.78802 6.138C6.78802 6.138 7.62502 5.869 9.53002 7.159C11.1639 6.71101 12.8882 6.71101 14.522 7.159C16.428 5.868 17.264 6.138 17.264 6.138C17.6336 6.97286 17.6694 7.91757 17.364 8.778C18.0376 9.50423 18.4045 10.4626 18.388 11.453C18.388 15.286 16.058 16.128 13.836 16.375C14.3153 16.8651 14.5612 17.5373 14.511 18.221C14.511 19.555 14.499 20.631 14.499 20.958C14.499 21.225 14.677 21.535 15.186 21.437C19.8265 19.8884 22.6591 15.203 21.874 10.3743C21.089 5.54565 16.9181 1.99888 12.026 2Z"></path>
+              </svg>
+            </a>
+          </div>
+
+          <p className="text-white font-[300] text-base mt-16">
             Legal Line Here
           </p>
         </div>
--- src/Sections/tempcode.jsx
+++ src/Sections/tempcode.jsx
@@ -6,6 +6,67 @@ import pattern1 from "./Assets/pattern1.jpg";
 function tempcode() {
   return (
     <div class="bg-gray-200">
+      <div class="flex flex-col items-center p-4 lg:p-8 transition-colors duration-300 transform border cursor-pointer rounded-xl hover:border-transparent group hover:bg-[#665d57] border-2 dark:border-gray-700 dark:hover:border-transparent">
+        <div class="flex mt-3 -mx-2">
+          <a
+            href="https://instagram.com/haren_py11a?igshid=Y2I2MzMwZWM3ZA=="
+            class="mx-2 text-gray-800 hover:text-gray-500 group-hover:text-white"
+          >
+            <svg
+              class="h-7 w-7"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              {" "}
+              <path stroke="none" d="M0 0h24v24H0z" />{" "}
+              <rect x="4" y="4" width="16" height="16" rx="4" />{" "}
+              <circle cx="12" cy="12" r="3" />{" "}
+              <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+            </svg>
+          </a>
+
+          <a
+            href="#n"
+            class="mx-2 text-gray-800 hover:text-gray-500 dark:hover:text-gray-300 group-hover:text-white"
+          >
+            <svg
+              class="h-6 w-6 "
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              {" "}
+              <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />{" "}
+              <rect x="2" y="9" width="4" height="12" />{" "}
+              <circle cx="4" cy="4" r="2" />
+            </svg>{" "}
+          </a>
+
+          <a
+            href="https://github.com/harenPy11a"
+            class="mx-2 text-gray-800 hover:text-gray-500 dark:hover:text-gray-300 group-hover:text-white"
+          >
+            <svg
+              class="w-6 h-6 fill-current"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M12.026 2C7.13295 1.99937 2.96183 5.54799 2.17842 10.3779C1.395 15.2079 4.23061 19.893 8.87302 21.439C9.37302 21.529 9.55202 21.222 9.55202 20.958C9.55202 20.721 9.54402 20.093 9.54102 19.258C6.76602 19.858 6.18002 17.92 6.18002 17.92C5.99733 17.317 5.60459 16.7993 5.07302 16.461C4.17302 15.842 5.14202 15.856 5.14202 15.856C5.78269 15.9438 6.34657 16.3235 6.66902 16.884C6.94195 17.3803 7.40177 17.747 7.94632 17.9026C8.49087 18.0583 9.07503 17.99 9.56902 17.713C9.61544 17.207 9.84055 16.7341 10.204 16.379C7.99002 16.128 5.66202 15.272 5.66202 11.449C5.64973 10.4602 6.01691 9.5043 6.68802 8.778C6.38437 7.91731 6.42013 6.97325 6.78802 6.138C6.78802 6.138 7.62502 5.869 9.53002 7.159C11.1639 6.71101 12.8882 6.71101 14.522 7.159C16.428 5.868 17.264 6.138 17.264 6.138C17.6336 6.97286 17.6694 7.91757 17.364 8.778C18.0376 9.50423 18.4045 10.4626 18.388 11.453C18.388 15.286 16.058 16.128 13.836 16.375C14.3153 16.8651 14.5612 17.5373 14.511 18.221C14.511 19.555 14.499 20.631 14.499 20.958C14.499 21.225 14.677 21.535 15.186 21.437C19.8265 19.8884 22.6591 15.203 21.874 10.3743C21.089 5.54565 16.9181 1.99888 12.026 2Z"></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+
       <div class="relative w-full h-96">
         <iframe
           class="absolute top-0 left-0 w-full h-full"
