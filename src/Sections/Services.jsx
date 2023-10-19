9fec0dc3d7b64ffdc710ce137d139a883bdd6f00
Sahil K <skhunt125@gmail.com>
2023-10-19 14:05:26 -0500

    savedProgress

--- src/Sections/Services.jsx
+++ src/Sections/Services.jsx
@@ -1,8 +1,240 @@
-import React from "react";
+// import React from "react";
 import Footer from "./Footer";
+import React, { useEffect } from "react";
+import Glide from "@glidejs/glide";
+import heroImg1 from "./Assets/heroImg1.jpg";
 
 function Services() {
-  return <div className="bg-blue-300 h-screen"></div>;
+  useEffect(() => {
+    const slider = new Glide(".glide-01", {
+      type: "carousel",
+      focusAt: "center",
+      perView: 3,
+      autoplay: 3000,
+      animationDuration: 700,
+      gap: 24,
+      classNames: {
+        nav: {
+          active: "[&>*]:bg-wuiSlate-700",
+        },
+      },
+      breakpoints: {
+        1024: {
+          perView: 2,
+        },
+        640: {
+          perView: 1,
+        },
+      },
+    }).mount();
+
+    return () => {
+      slider.destroy();
+    };
+  }, []);
+
+  return (
+    <div className="bg-blue-300 h-screen pt-36">
+      <>
+        {/*<!-- Component: Carousel with controls inside --> */}
+        <div className="glide-01 relative w-fu">
+          {/*    <!-- Slides --> */}
+
+          <div data-glide-el="track">
+            <ul className="whitespace-no-wrap flex-no-wrap [backface-visibility: hidden] [transform-style: preserve-3d] [touch-action: pan-Y] [will-change: transform] relative flex w-full overflow-hidden p-0 pb-12">
+              <li>
+                <div className="h-full w-full">
+                  {/*                    <!-- Start Testimonial --> */}
+
+                  <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                      <img class="rounded-t-lg" src={heroImg1} alt="" />
+                    </a>
+                    <div class="p-5">
+                      <a href="#">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                          Noteworthy technology acquisitions 2021
+                        </h5>
+                      </a>
+                      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        Here are the biggest enterprise technology acquisitions
+                        of 2021 so far, in reverse chronological order.
+                      </p>
+                      <a
+                        href="#"
+                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                      >
+                        Read more
+                        <svg
+                          class="w-3.5 h-3.5 ml-2"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 14 10"
+                        >
+                          <path
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M1 5h12m0 0L9 1m4 4L9 9"
+                          />
+                        </svg>
+                      </a>
+                    </div>
+                  </div>
+
+                  {/*                    <!-- End Testimonial --> */}
+                </div>
+              </li>
+              <li>
+                <div className="h-full w-full">
+                  {/*                    <!-- Start Testimonial --> */}
+
+                  <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                      <img class="rounded-t-lg" src={heroImg1} alt="" />
+                    </a>
+                    <div class="p-5">
+                      <a href="#">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                          Noteworthy technology acquisitions 2021
+                        </h5>
+                      </a>
+                      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        Here are the biggest enterprise technology acquisitions
+                        of 2021 so far, in reverse chronological order.
+                      </p>
+                      <a
+                        href="#"
+                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                      >
+                        Read more
+                        <svg
+                          class="w-3.5 h-3.5 ml-2"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 14 10"
+                        >
+                          <path
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M1 5h12m0 0L9 1m4 4L9 9"
+                          />
+                        </svg>
+                      </a>
+                    </div>
+                  </div>
+
+                  {/*                    <!-- End Testimonial --> */}
+                </div>
+              </li>
+              <li>
+                <div className="h-full w-full">
+                  {/*                    <!-- Start Testimonial --> */}
+
+                  <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                      <img class="rounded-t-lg" src={heroImg1} alt="" />
+                    </a>
+                    <div class="p-5">
+                      <a href="#">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                          Noteworthy technology acquisitions 2021
+                        </h5>
+                      </a>
+                      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        Here are the biggest enterprise technology acquisitions
+                        of 2021 so far, in reverse chronological order.
+                      </p>
+                      <a
+                        href="#"
+                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                      >
+                        Read more
+                        <svg
+                          class="w-3.5 h-3.5 ml-2"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 14 10"
+                        >
+                          <path
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M1 5h12m0 0L9 1m4 4L9 9"
+                          />
+                        </svg>
+                      </a>
+                    </div>
+                  </div>
+
+                  {/*                    <!-- End Testimonial --> */}
+                </div>
+              </li>
+            </ul>
+          </div>
+
+          {/*    <!-- Controls --> */}
+          <div
+            className="absolute left-0 top-1/2 flex h-0 w-full items-center justify-between px-4 "
+            data-glide-el="controls"
+          >
+            <button
+              className="inline-flex h-8 w-8 items-center justify-center rounded-full border border-slate-700 bg-white/20 text-slate-700 transition duration-300 hover:border-slate-900 hover:text-slate-900 focus-visible:outline-none lg:h-12 lg:w-12"
+              data-glide-dir="<"
+              aria-label="prev slide"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-5 w-5"
+              >
+                <title>prev slide</title>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+                />
+              </svg>
+            </button>
+            <button
+              className="inline-flex h-8 w-8 items-center justify-center rounded-full border border-slate-700 bg-white/20 text-slate-700 transition duration-300 hover:border-slate-900 hover:text-slate-900 focus-visible:outline-none lg:h-12 lg:w-12"
+              data-glide-dir=">"
+              aria-label="next slide"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-5 w-5"
+              >
+                <title>next slide</title>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.0.2/glide.js"></script>
+        {/*<!-- End Carousel with controls inside --> */}
+      </>
+    </div>
+  );
 }
 
 export default Services;
