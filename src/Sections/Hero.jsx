46ef9a7a49f90896da79258fcfed864f2ad4aea4
Vedant P <vpat1621@gmail.com>
2023-12-22 15:02:53 -0600

    Update Hero.jsx

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -4,7 +4,11 @@ function Hero() {
   return (
     <div className="h-[100vh] bg-blue-200 ">
       <div className="">
-        <img class="z-0 w-full" src={heroImg1} alt="Image Description" />
+        <img
+          class="invisible md:visible md:mt-[3%] z-0 w-full"
+          src={heroImg1}
+          alt="Image Description"
+        />
         <div class="z-10 absolute top-[26%] right-[7%]">
           <h2 class="text-8xl font-extrabold text-gray-800">LifeSciences</h2>
           <p class="max-w-[650px] my-4 text-xl font-italic text-white">
